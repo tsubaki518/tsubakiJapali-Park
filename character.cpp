85c77233a315c97f1baf64e501781944cc79f418
harentiman <harentiman@gmail.com>
2019-11-08 17:42:08 +0900

    キャラ生成段階のif文、仮打ちです。

--- character.cpp
+++ character.cpp
@@ -5,25 +5,42 @@ void Character::Draw() {
 }
 
 
-
-
 //if‚Åweight‚É’l‚Ì”ÍˆÍ‚ğw’è‚µ‚Äreturn‚³‚¹‚éƒLƒƒƒ‰‚ğŒˆ‚ß‚é
 Character SetCharacter(float weight) {
 	Character character;
-	Mouse mouse;
-	Elephant elephant;
-	Bear bear;
-	Rabbit rabbit;
-	Dog dog;
+	Elephant elephant;			//90kgˆÈã
+	Bear bear;					//70`89kg
+	Dog dog;					//60`69kg
+	Rabbit rabbit;				//50`59kg
+	Mouse mouse;				//49kgˆÈ‰º
+	
 
 	//--------------------------------
 	//    ‚±‚±‚ÅƒLƒƒƒ‰‚ğŒˆ’è‚·‚é
 	//-------------------------------
 
-
+	if (weight <= 90)
+	{
+		return elephant;
+	}
+	else if (weight >= 89)
+	{
+		return bear;
+	}
+	else if (weight >= 69)
+	{
+		return dog;
+	}
+	else if (weight >= 59)
+	{
+		return rabbit;
+	}
+	else if (weight >= 49)
+	{
+		return mouse;
+	}
 
 	//---------------------------------
-
 
 
 	//ğŒ‚É“–‚Ä‚Í‚Ü‚ç‚È‚¢ê‡character‚ğreturn‚³‚¹‚é
