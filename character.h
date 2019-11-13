eaaad9de00b1399b6c80d6cf95c8a8d289f34eeb
harentiman <harentiman@gmail.com>
2019-11-13 14:24:02 +0900

    キャラ生成の編集。 キャラクタの各パラメータを追加。 各キャラのパラメータの仮数値を設定。

--- character.h
+++ character.h
@@ -1,49 +1,93 @@
 #pragma once
 
+//各パラメータの基準値
+#define NORMAL_MAX_SPEED 1
+#define NORMAL_MOVE_ACCEL 1
+#define NORMAL_HANDLING 1
+#define NORMAL_DEFENSE 1
+#define NORMAL_SPIN_NUM 1
+
+
 class Character {
 protected:
-	float moveAccel=0;
-	float rotateSpeed=0;
+	float maxSpeed = 0;			//最大速度
+	float moveAccel = 0;		//加速性能
+	float handling = 0;			//ハンドリング性能
+	float defense = 0;			//弾き耐性
+	int	spinNum = 0;			//スピン持続
 public:
 	Character(){}
 	virtual void Draw();
 
 	Character operator=(Character c) {
+		maxSpeed = c.maxSpeed;
 		moveAccel = c.moveAccel;
-		rotateSpeed = c.rotateSpeed;
-
+		handling = c.handling;
+		defense = c.defense;
+		spinNum = c.spinNum;
 		return *this;
 	}
 };
 
 
+//各キャラクタのパラメータ
 class Elephant :public Character {
 public:
-	Elephant(){}
+	Elephant(){
+		maxSpeed = NORMAL_MAX_SPEED * 1.04;
+		moveAccel = NORMAL_MOVE_ACCEL * 1.08;
+		handling = NORMAL_HANDLING * 0.90;
+		defense = NORMAL_DEFENSE * 1.10;
+	  //spinNum= NORMAL_SPIN_NUM
+	}
 	void Draw(){}
 };
 
 class Bear :public Character {
 public:
-	Bear(){}
+	Bear(){
+		maxSpeed = NORMAL_MAX_SPEED * 1.02;
+		moveAccel = NORMAL_MOVE_ACCEL * 1.04;
+		handling = NORMAL_HANDLING * 0.92;
+		defense = NORMAL_DEFENSE * 1.06;
+	  //spinNum= NORMAL_SPIN_NUM
+	}
 	void Draw(){}
 };
 
 class Dog :public Character {
 public:
-	Dog(){}
+	Dog(){
+		maxSpeed = NORMAL_MAX_SPEED * 1.00;
+		moveAccel = NORMAL_MOVE_ACCEL * 1.00;
+		handling = NORMAL_HANDLING * 0.94;
+		defense = NORMAL_DEFENSE * 1.00;
+	  //spinNum= NORMAL_SPIN_NUM
+	}
 	void Draw(){}
 };
 
 class Rabbit :public Character {
 public:
-	Rabbit(){}
+	Rabbit(){
+		maxSpeed = NORMAL_MAX_SPEED * 0.96;
+		moveAccel = NORMAL_MOVE_ACCEL * 0.96;
+		handling = NORMAL_HANDLING * 0.96;
+		defense = NORMAL_DEFENSE * 0.90;
+	  //spinNum= NORMAL_SPIN_NUM
+	}
 	void Draw(){}
 };
 
 class Hamster :public Character {
 public:
-	Hamster(){}
+	Hamster(){
+		maxSpeed = NORMAL_MAX_SPEED * 0.94;
+		moveAccel = NORMAL_MOVE_ACCEL * 0.88;
+		handling = NORMAL_HANDLING * 1.00;
+		defense = NORMAL_DEFENSE * 0.74;
+	  //spinNum= NORMAL_SPIN_NUM
+	}
 	void Draw(){}
 };
 
