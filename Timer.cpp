e284b43ddcb53243e4a84e7b327912ecf260d78b
BANTERIN <1951kono@gmail.com>
2019-11-20 13:04:17 +0900

    時間計測

--- Timer.cpp
+++ Timer.cpp
@@ -1,19 +1,32 @@
+#include<stdio.h>
 #include"Timer.h"
 #include<time.h>
 #include"debug_font.h"
-
-
+double jikan;
+clock_t start, end;
 
 void TimerInit() {
+	 start = clock();
+
+	
+
+	
+	
 
 }
 
 //時間の計測
 void TimerUpdate() {
-
+ end = clock();
+ jikan = (end - start) / CLOCKS_PER_SEC ;
 }
+
+ 
+
+	
+
 
 //引数で指定した座標に時間を表示する
 void TimerDraw(float x, float y) {
-
+	DebugFont_Draw(x,y,"%d",(int)jikan);
 }
