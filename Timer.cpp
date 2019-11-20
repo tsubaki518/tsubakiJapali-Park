933cc220fa035bfcbf9c82fdc6dc6fc5a7bc7df9
BANTERIN <1951kono@gmail.com>
2019-11-20 17:49:56 +0900

    わかりやすいよ♡時間だよ♡

--- Timer.cpp
+++ Timer.cpp
@@ -1,34 +1,29 @@
-#include<stdio.h>
 #include"Timer.h"
 #include<time.h>
 #include"debug_font.h"
-double jikan;
+double byou;
+int hun;
+
 static clock_t start, end;
-	
 
-void TimerInit() {
+
+void TimerInit() 
+{
 	 start = clock();
-  
-	
-
-	
-	
-
 }
 
 //時間の計測
 void TimerUpdate() {
  end = clock();
- jikan = (end - start) / (double)CLOCKS_PER_SEC ;
-
+ byou = (end - start) / (double)CLOCKS_PER_SEC ;
+ if (byou >= 60) {
+	 start = clock();
+	 byou = 0;
+	 hun++;
+ }
 }
-
- 
-
-	
-
 
 //引数で指定した座標に時間を表示する
 void TimerDraw(float x, float y) {
-	DebugFont_Draw((int)x,(int)y,"%0.3lf",(float)jikan);
+	DebugFont_Draw((int)x,(int)y,"%d分%0.3lf秒", hun,(float)byou);
 }
