553b46f9a7eb78b13be0ab30eb72233fa56ae739
nanasesakamoto <mknn797.gmail.com>
2019-11-20 14:24:50 +0900

    加点がない状態で2秒経過時マイナス(rating.cppに追加)

--- Rating.cpp
+++ Rating.cpp
@@ -7,14 +7,16 @@ float rating;
 //秒間処理
 static clock_t start,timer, end;
 
-bool sorispin = false;//スピン判定
-bool hitcrimp = false;//障害物当たる
+static bool sorispin = false;//スピン判定
+static bool hitcrimp = false;//障害物当たる
+static int cnt;//2秒間カウント
 
 //視聴率の初期化
 void RatingInit() {
 	rating = 5.0f;
 	start = NULL;
 	sorispin = false;
+	cnt = 0;
 }
 
 //視聴率の計測
@@ -40,17 +42,20 @@ void RatingUpdate(Sori sori) {
 	end = clock();
 	if (end - start >= timer) {
 		start += timer;
+		cnt += 1;
 
 		//自機が高スピードを維持し続ける。80%以上の時
 		if (sori.speed >= sori.maxSpeed*0.8) {
 			rating += 0.5f;
+			cnt = 0;
 		}
-		/*
+		
 		//加点が無い状態が2秒以上継続。
-		if () {
-		rating -= 0.1f;
+		if (cnt >= 2) {
+			rating -= 0.1f;
 		}
 
+		/*
 		//自分の順位が3位以上の間。
 		if () {
 
@@ -60,6 +65,7 @@ void RatingUpdate(Sori sori) {
 		if (sorispin == true) {
 			rating += 0.6f;
 			sorispin = false;
+			cnt = 0;
 		}
 		
 	}
@@ -69,23 +75,27 @@ void RatingUpdate(Sori sori) {
 	if (hitcrimp == true) {
 		rating += 0.05f;
 		hitcrimp = false;
+		cnt = 0;
 	}
 
 	/*
 	//自機が他機体にぶつかる。
 	if () {
 	rating += 0.1f;
+	cnt = 0;
 	}
 
 
 	//他のプレイヤーが脱落する。
 	if () {
 	rating += 5.0f;
+	cnt = 0;
 	}
 
 	//自分の順位が繰り上がる。
 	if () {
 	rating += 3.0f;
+	cnt = 0;
 	}
 
 
