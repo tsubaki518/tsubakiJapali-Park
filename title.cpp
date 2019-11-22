46ef95d8a1dd77f3c305268c2c43956a39bda046
nanasesakamoto <mknn797.gmail.com>
2019-11-22 16:21:58 +0900

    titleのロゴ以外の設定

--- title.cpp
+++ title.cpp
@@ -5,21 +5,21 @@
 #include"XInput.h"
 #include"main.h"
 #include"debug_font.h"
+#include <time.h>
 
 static float cursorPos = PosGame;
-float t = 0;
-float posX = 0;
-float posY = 0;
+static float t = 0;
+static float posX = 0;
+static float posY = 0;
 
 void TitleInit() {
 	
 }
 
 void TitleUpdate() {
-
 	//ÉJÅ[É\ÉãÇÃà⁄ìÆ
 	bool XinputRightJoystickDown(int direction);
-	if (Keyboard_IsTrigger(DIK_A) || Keyboard_IsTrigger(DIK_LEFTARROW) || XinputPressButtonDown(LEFT_BUTTON)) {
+	if (Keyboard_IsTrigger(DIK_D) || Keyboard_IsTrigger(DIK_RIGHTARROW) || XinputPressButtonDown(RIGHT_BUTTON)) {
 		if (cursorPos == PosGame) {
 			cursorPos = PosDate;
 
@@ -28,7 +28,7 @@ void TitleUpdate() {
 			cursorPos--;
 		}
 	}
-	else if (Keyboard_IsTrigger(DIK_D) || Keyboard_IsTrigger(DIK_RIGHTARROW) || XinputPressButtonDown(RIGHT_BUTTON)) {
+	else if (Keyboard_IsTrigger(DIK_A) || Keyboard_IsTrigger(DIK_LEFTARROW) || XinputPressButtonDown(LEFT_BUTTON)) {
 		if (cursorPos == PosDate) {
 			cursorPos = PosGame;
 
@@ -60,12 +60,22 @@ void TitleUpdate() {
 void TitleDraw() {
 	Sprite_SetColor(D3DCOLOR_RGBA(255, 255, 255, 255));
 	Sprite_Draw(TEXTURE_INDEX_TITLE, 0, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-	DebugFont_Draw(SCREEN_WIDTH / 2 -50, SCREEN_HEIGHT / 2 + 120, "START");
+
+	if (cursorPos == PosGame) {
+		Sprite_Draw(TEXTURE_INDEX_TITLE, SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 + 120, 0, 0, 80, 25);
+	}
+	if (cursorPos == PosOption) {
+		Sprite_Draw(TEXTURE_INDEX_TITLE, SCREEN_WIDTH / 2 - 300, SCREEN_HEIGHT / 2 + 120, 0, 0, 100, 25);
+	}
+	if (cursorPos == PosDate) {
+		Sprite_Draw(TEXTURE_INDEX_TITLE, SCREEN_WIDTH / 2 + 200, SCREEN_HEIGHT / 2 + 120, 0, 0, 130, 25);
+	}
+
+	DebugFont_Draw(SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 + 120, "START");
 	DebugFont_Draw(SCREEN_WIDTH / 2 - 300, SCREEN_HEIGHT / 2 + 120, "OPTIONS");
 	DebugFont_Draw(SCREEN_WIDTH / 2 + 200, SCREEN_HEIGHT / 2 + 120, "DATEBASE");
 
 }
 
 void TitleUnInit() {
-
 }
