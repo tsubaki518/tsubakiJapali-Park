bb6bb0b0639fee65ff914946679a7d7e8a2daf7b
harentiman <harentiman@gmail.com>
2019-11-28 16:45:40 +0900

    ステージ生成。 α版ステージの修正、加速床を追加。 ※加速判定無し。杉本氏に引き継ぎ予定。

--- Stage.h
+++ Stage.h
@@ -5,11 +5,14 @@ void StageDraw();
 
 //---ˆø”‚Åw’è‚µ‚½cube‚ğ“¾‚é
 Cube GetCube(int n);
+Cube GetAccelSpeedCube(int n);
 Cube GetRightWall(int n);
 Cube GetLeftWall(int n);
 Cube GetGoalCube();
 
+
 //---g‚Á‚Ä‚¢‚écube‚Ì”‚ğ“¾‚é
 int GetCubeNum();
+int GetAccelSpeedNum();
 int GetRightWallNum();
 int GetLeftWallNum();
