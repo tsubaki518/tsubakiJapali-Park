20df6dba56f3c25bd7208712b810d3b04be8731b
BANTERIN <1951kono@gmail.com>
2019-12-04 11:54:05 +0900

    石井くんの時間テクスチャ

--- Timer.cpp
+++ Timer.cpp
@@ -1,6 +1,8 @@
 #include"Timer.h"
 #include<time.h>
 #include"debug_font.h"
+#include"ImageNumber.h"
+#include<d3dx9.h>
 double byou;
 int hun;
 
@@ -25,5 +27,6 @@ void TimerUpdate() {
 
 //ˆø”‚Åw’è‚µ‚½À•W‚ÉŠÔ‚ğ•\¦‚·‚é
 void TimerDraw(float x, float y) {
-	DebugFont_Draw((int)x,(int)y,"%d•ª%0.3lf•b", hun,(float)byou);
+	ImageNumberDraw(D3DXVECTOR2(1000, 10), D3DXVECTOR2(0.3,0.3), (int)byou);
+	ImageNumberDraw(D3DXVECTOR2(5, 5), D3DXVECTOR2(0.3,0.3), (int)hun);
 }
