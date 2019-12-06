cab0a2f3d49cad607b743e1f0e870dd43e5ca1a1
BANTERIN <1951kono@gmail.com>
2019-12-06 17:30:24 +0900

    時間表示(小数点２位)

--- Timer.cpp
+++ Timer.cpp
@@ -3,8 +3,12 @@
 #include"debug_font.h"
 #include"ImageNumber.h"
 #include<d3dx9.h>
+#include"common.h"
+#include<math.h>
 double byou;
 int hun;
+float seisuu;
+float syousuu;
 
 static clock_t start, end;
 
@@ -16,17 +20,34 @@ void TimerInit()
 
 //時間の計測
 void TimerUpdate() {
- end = clock();
- byou = (end - start) / (double)CLOCKS_PER_SEC ;
- if (byou >= 60) {
-	 start = clock();
-	 byou = 0;
-	 hun++;
+	end = clock();
+	byou = (end - start) / (double)CLOCKS_PER_SEC;
+	if (byou >= 60) {
+		start = clock();
+		byou = 0;
+		hun++;
+	}
+	syousuu = byou - (int)byou;
+	seisuu = (int)byou;
+	
+	syousuu *= 100;
  }
-}
 
 //引数で指定した座標に時間を表示する
 void TimerDraw(float x, float y) {
-	ImageNumberDraw(D3DXVECTOR2(1000, 10), D3DXVECTOR2(0.3,0.3), (int)byou);
-	ImageNumberDraw(D3DXVECTOR2(5, 5), D3DXVECTOR2(0.3,0.3), (int)hun);
+	//少数の表示
+	if (syousuu < 10) {
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 200, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), 0);
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 400, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
+
+	}
+	else if (syousuu >= 10) {
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 400, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
+	}
+	//秒の表示
+	ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH*5/12*11., SCREEN_HEIGHT*5/18), D3DXVECTOR2(0.2,0.2), (int)seisuu);
+
+	//分の表示
+	ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH*5/12*10.5, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2,0.2), (int)hun);
+	
 }
