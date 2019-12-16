f2d4592a3e3baf767e76fbed49b51fcab867d638
BANTERIN <1951kono@gmail.com>
2019-12-16 16:44:44 +0900

    計測座標　

--- Timer.cpp
+++ Timer.cpp
@@ -16,6 +16,9 @@ static clock_t start, end;
 void TimerInit() 
 {
 	 start = clock();
+	 hun = 0;
+	 byou = 0;
+	 syousuu = 0;
 }
 
 //時間の計測
@@ -31,25 +34,33 @@ void TimerUpdate() {
 	seisuu = (int)byou;
 	
 	syousuu *= 100;
- }
+ 
+}
 
 //引数で指定した座標に時間を表示する
 void TimerDraw(float x, float y) {
 	//少数の表示
 	if (syousuu < 10) {
-		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 200, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), 0);
-		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 400, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 200, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), 0);
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 10.98 + 400, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
 
 	}
 	else if (syousuu >= 10) {
-		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 400, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 11 + 400, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), (int)syousuu);
 	}
 	//秒の表示
-	ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH*5/12*11., SCREEN_HEIGHT*5/18), D3DXVECTOR2(0.2,0.2), (int)seisuu);
+	if (byou < 10) {
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 11.79* 10.6, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), 0);
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 11.79 * 10.8, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), (int)seisuu);
+	}
+	else if (byou >= 10)
+	{
+		ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 11.79 * 10.8, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), (int)seisuu);
+	}
 
 	//分の表示
-	ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH*5/12*10.5, SCREEN_HEIGHT * 5 / 18), D3DXVECTOR2(0.2,0.2), (int)hun);
-	
+	ImageNumberDraw(D3DXVECTOR2(SCREEN_WIDTH * 5 / 12 * 10.5, SCREEN_HEIGHT * 5 / 20), D3DXVECTOR2(0.2, 0.2), (int)hun);
+
 }
 
 float GetTime() {
