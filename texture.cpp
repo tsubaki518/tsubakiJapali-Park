acee85fb7dd6106e16f6568146f6fef147a96a2b
sugimotokento <keyunutoma@gmail.com>
2020-01-30 10:02:31 +0900

    ssettingsceneのテクスチャ変更

--- texture.cpp
+++ texture.cpp
@@ -46,12 +46,12 @@ static const TextureFile g_TextureFiles[] = {
 
 {"asset/texture/ソクテイカンリョウ!.png",717,65},
 {"asset/texture/ソクテイチュウ.png",717,65},
-{"asset/texture/そのままボードの上で～.png",594,107},
+{"asset/texture/ボードの上に乗り.png",594,107},
 {"asset/texture/どちらかがジャンプすると～.png",594,107},
 {"asset/texture/プレイヤーエントリー.png",717,65},
 {"asset/texture/プレイヤーを探しています.png",369,135},
 {"asset/texture/共通背景.png",1920,1080},
-{"asset/texture/参加プレイヤーを認識~.png",353,108},
+{"asset/texture/参加プレイヤーを認識.png",353,108},
 {"asset/texture/情報アイコン.png",158,162},
 {"asset/texture/立ち絵その１.png",366,475},
 
